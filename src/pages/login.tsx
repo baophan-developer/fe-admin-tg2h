d8cc92257a1ba8538ab4b327b5552d54f9b4d043
baophan-developer <baophan.developer@gmail.com>
2023-09-29 12:17:48 +0700

    create login layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import LoginLayout from "@/layouts/LoginLayout";
 
 export default function Login() {
     return <div>This is login page</div>;
 }
+
+Login.getLayout = function getLayout(page: React.ReactElement) {
+    return <LoginLayout>{page}</LoginLayout>;
+};
